4aa0495eb97e70054e757abf88997b3534eb1627
andres <condeandres34@gmail.com>
2019-10-31 16:04:20 -0300

    se sube el archivo

--- registro.php
+++ registro.php
@@ -1,12 +1,23 @@
 <?php
 // errores-------------------------------------------------------------------
 $errores=[
+  "email-log"=>"",
+  "password-log"=>"",
   "email"=>"",
   "password"=>"",
   "direccion"=>"",
   "provincia"=>"",
   "usu-ya-regis"=>""
 ];
+if ($_POST) {
+    if (filter_var($_POST["email-log"], FILTER_VALIDATE_EMAIL)== false) {
+    $errores["email"]="El mail no tiene el formato correcto<br>";
+  }
+
+  if (strlen($_POST["password-log"])<8){
+    $errores["password"]="Su Contraseña debe tener almenos 8 caracteres<br>";
+  }
+}
 if ($_POST) {
     if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)== false) {
     $errores["email"]="El mail no tiene el formato correcto<br>";
@@ -64,8 +75,17 @@ if($_POST){
   file_put_contents("users.json", $json);
   }
 }
-var_dump($_FILES)
-
+var_dump($_FILES);
+function upload($name, $dir){
+  if ($_FILES[$name]['error'] === UPLOAD_ERR_OK) {
+    $ext = pathinfo($_FILES[$name]['name'], PATHINFO_EXTENSION);
+    $hash =crypt($_FILES[$name]['name'], random_bytes(10));
+    move_uploaded_file($_FILES[$name]['tmp_name'], "$dir/$hash.$ext");
+  }
+}
+  if ($_FILES) {
+    upload("fotode-perfil","archivos");
+  }
  ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -82,7 +102,73 @@ var_dump($_FILES)
   <title>Registrarme!</title>
 </head>
 <body>
+  <header>
+    <!-- NABVAR -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between">
+      <a class="navbar-brand" href="index.php">Logo</a>
 
+      <div class="d-flex justify-content-end" id="navbarSupportedContent">
+
+        <!-- BOTON DE LOGIN     -->
+        <form class="form-inline my-2 my-lg-0">
+
+          <button type="button" class="btn btn-outline-dark" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Acceder</button>
+
+        </form>
+
+        <!-- FORMULARIO DE LOGIN -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Ingresar</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <form  action="" method="post">
+
+                  <div class="form-group">
+                    <label for="recipient-name" class="col-form-label">Correo Electronico:</label>
+                    <input type="text" class="form-control" id="recipient-name" name="email" value="<?=$email?>">
+                    <span><?= $errores["email"]?></span>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="recipient-name" class="col-form-label">Contraseña:</label>
+                    <input type="password" class="form-control" id="recipient-name" name="password" value="<?=$password?>">
+                    <span><?= $errores["password"]?></span>
+                  </div>
+
+                  <div class="d-flex justify-content-between">
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" id="gridCheck">
+                        <label class="form-check-label" for="gridCheck">
+                          Recordarme
+                        </label>
+                    </div>
+
+                    <div class="form-group">
+                      <a href="registro.php">Registrarme</a>
+                    </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                    <button type="submit" class="btn btn-outline-dark">Aceptar</button>
+                  </div>
+                </form>
+              </div>
+
+
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </nav>
+  </header>
   <div class="d-flex justify-content-center my-5">
 
     <form action="registro.php" method="post" enctype="multipart/form-data">
@@ -127,12 +213,12 @@ var_dump($_FILES)
               <option>Uruguay</option>
             </select>
           </div>
-          <div class="">
-            <label for="avatar">Adjuntar foto de perfil<br></label>
-            <input type="file" name="fotode-perfil" value="">
+          <div class="col-xl-12">
+            <label for="avatar" class="col-xl-12">Adjuntar foto de perfil<br></label>
+            <input type="file" name="fotode-perfil" value="" class="m-2">
           </div>
-          <label for=""><br></label>
-        <button type="submit" class="btn btn-outline-dark align-items-end">Registrarme</button>
+          <button type="submit" class="btn btn-outline-dark align-items-end ml-3">Registrarme</button>
+
     </form>
 
   </div>
